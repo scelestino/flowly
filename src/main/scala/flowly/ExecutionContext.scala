233fff952db91608f120375505a9cf65aed42682
Sebastian <scelestino@despegar.com>
2018-09-03 18:46:22 -0300

    execution context draft

--- src/main/scala/flowly/ExecutionContext.scala
+++ src/main/scala/flowly/ExecutionContext.scala
@@ -1,6 +1,44 @@
 package flowly
 
-class ExecutionContext(val sessionId:String, variables: Map[String, Any]) {
+/**
+  * Interface used by [[BlockingTask]] and [[DisjunctionTask]]
+  */
+trait ReadableExecutionContext {
+
+  def sessionId:String
+
+  def get[T](key:String):Option[T]
+
+  def getOrElse[T](key:String, f: => T):T
+
+  def contains(key:String):Boolean
+
+  def exists[T](key:String, f:T => Boolean):Boolean
+
+}
+
+/**
+  * Interface used by [[ExecutionTask]]
+  */
+trait WriteableExecutionContext extends ReadableExecutionContext {
+
+  def set[T](key:String, value:T):WriteableExecutionContext
+
+  def unset(key:String):WriteableExecutionContext
+
+}
+
+/**
+  * Immutable context that is pass through the tasks during a workflow execution.
+  * It can be used from a [[BlockingTask]], [[DisjunctionTask]] or [[ExecutionTask]] in order to make decisions or
+  * it can be modified (through a copy) from a [[ExecutionTask]].
+  *
+  * After a successful task this context is saved in the repository in order to keep safe any modification.
+  *
+  * @param sessionId workflow instance id
+  * @param variables variables stored in the workflow
+  */
+class ExecutionContext(val sessionId:String, variables: Map[String, Any]) extends WriteableExecutionContext {
 
   def get[T](key:String):Option[T] = variables.get(key).asInstanceOf[Option[T]]
 
--- src/main/scala/flowly/Task.scala
+++ src/main/scala/flowly/Task.scala
@@ -1,150 +1,190 @@
 package flowly
+
 import org.json4s.JValue
 import org.json4s.JsonAST.JString
 import org.json4s.scalaz.JsonScalaz
 
 
-
 trait Task {
 
-  def id:String
+  def id: String
 
-  def path(ctx:ExecutionContext):Stream[Task]
+  def path(ctx: ExecutionContext): Stream[Task]
 
-  def perform(ctx:ExecutionContext):Result
+  def perform(ctx: ExecutionContext): Result
+
+  private def tasks(previous: List[Task]): List[Task] = {
+    if (previous.contains(this)) previous
+    else followedBy.foldLeft(this :: previous) {
+      (seed, task) => task.tasks(seed)
+    }
+  }
+  def tasks: List[Task] = tasks(Nil)
+
+  def followedBy: List[Task]
+
+  override def toString: String = s"Task:$id"
 
 }
 
 trait BaseTask extends Task {
 
-  def next:Task
+  def next: Task
 
-  def path(ctx:ExecutionContext): Stream[Task] = this #:: next.path(ctx)
+  def path(ctx: ExecutionContext): Stream[Task] = this #:: next.path(ctx)
+
+  def followedBy: List[Task] = next :: Nil
 
 }
 
 trait DisjunctionTask extends Task {
 
-  def branches: List[(ExecutionContext => Boolean, Task)]
+  def branches: List[(ReadableExecutionContext => Boolean, Task)]
 
-  def path(ctx:ExecutionContext): Stream[Task] = next(ctx) match {
+  def path(ctx: ExecutionContext): Stream[Task] = next(ctx) match {
     case Some(task) => this #:: task.path(ctx)
-    case None       => this #:: Stream.empty[Task]
+    case None => this #:: Stream.empty[Task]
   }
 
-  def perform(ctx:ExecutionContext): Result = {
+  def perform(ctx: ExecutionContext): Result = {
     next(ctx) match {
       case Some(task) => Ok()
-      case None       => Error()
+      case None => Error()
     }
   }
 
-  private def next(ctx:ExecutionContext):Option[Task] = branches.collectFirst { case (condition, task) if condition(ctx) => task }
+  def followedBy: List[Task] = branches.collect { case (_, task) => task }
+
+  private def next(ctx: ExecutionContext): Option[Task] = branches.collectFirst { case (condition, task) if condition(ctx) => task }
 
 }
 
 trait FinishTask extends Task {
 
-  def path(ctx:ExecutionContext): Stream[Task] = Stream(this)
+  def path(ctx: ExecutionContext): Stream[Task] = Stream(this)
 
   def perform(ctx: ExecutionContext): Result = {
     println(s"FINISH! $id")
     Finish()
   }
 
+  def followedBy: List[Task] = Nil
+
 }
 
 trait BlockingTask extends BaseTask {
 
-  def condition(ctx:ExecutionContext):Boolean
+  def condition(ctx: ReadableExecutionContext): Boolean
 
-  def perform(ctx:ExecutionContext): Result = if(condition(ctx)) Ok() else Blocked()
+  def perform(ctx: ExecutionContext): Result = if (condition(ctx)) Ok() else Blocked()
 
 }
 
 trait ExecutionTask extends BaseTask {
 
-  def perform(ctx:ExecutionContext): Result = try {
+  def perform(ctx: ExecutionContext): Result = try {
     execute(ctx) match {
-      case ExecutionOk    => Ok()
+      case ExecutionOk => Ok()
       case ExecutionError => Error()
     }
   } catch {
-    case e:Throwable => Error()
+    case e: Throwable => Error()
   }
 
-  protected def execute(ctx:ExecutionContext):ExecutionResult
+  protected def execute(ctx: WriteableExecutionContext): ExecutionResult
 
   trait ExecutionResult
+
   object ExecutionOk extends ExecutionResult
+
   object ExecutionError extends ExecutionResult
 
 }
 
 
-
 object Main extends App {
 
-  val finish = new FinishTask {
-    def id: String = "3"
-  }
-
-  val finish2 = new FinishTask {
-    def id: String = "4"
-    def perform: Result = {
-      println("FINISH2!!!")
-      Finish()
+  trait Finish1Component {
+    val finish:Task = new FinishTask {
+      def id: String = "3"
     }
   }
 
-  val blocking = new BlockingTask {
+  trait Finish2Component {
+    val finish2:Task = new FinishTask {
+      def id: String = "4"
 
-    def condition(ctx:ExecutionContext):Boolean = true
-
-    def next: Task = finish
-
-    def id: String = "BLOCKING!"
-
-  }
-
-  val second = new ExecutionTask {
-
-    def next: Task = blocking
-
-    def id: String = "2"
-
-    def execute(ctx:ExecutionContext): ExecutionResult = {
-      println("executing 2")
-      ExecutionOk
+      def perform: Result = {
+        println("FINISH2!!!")
+        Finish()
+      }
     }
-
   }
 
-  val conditional = new DisjunctionTask {
+  trait BlockingComponent {
+    this: ConditionalComponent =>
+    val blocking:Task = new BlockingTask {
 
-    def branches: List[(ExecutionContext => Boolean, Task)] = ((ctx:ExecutionContext) => ctx.sessionId == "AAA", finish2) :: ((ctx:ExecutionContext) => true, second) :: Nil
+      def condition(ctx: ReadableExecutionContext): Boolean = true
 
-    def id: String = "disjunction"
+      def next: Task = conditional
 
-  }
+      def id: String = "BLOCKING!"
 
-  val first = new ExecutionTask {
-
-    def next: Task = conditional
-
-    def id: String = "1"
-
-    def execute(ctx:ExecutionContext): ExecutionResult = {
-      println("this is the 1")
-      ExecutionOk
     }
-
   }
+
+  trait SecondComponent {
+    this: BlockingComponent =>
+    val second:Task = new ExecutionTask {
+
+      def next: Task = blocking
+
+      def id: String = "2"
+
+      def execute(ctx: WriteableExecutionContext): ExecutionResult = {
+        println("executing 2")
+        ExecutionOk
+      }
+
+    }
+  }
+
+  trait ConditionalComponent {
+    this: Finish2Component with SecondComponent =>
+    val conditional:Task = new DisjunctionTask {
+
+      def branches: List[(ReadableExecutionContext => Boolean, Task)] = ((ctx: ReadableExecutionContext) => ctx.sessionId == "BBB", finish2) :: ((ctx: ReadableExecutionContext) => true, second) :: Nil
+
+      def id: String = "disjunction"
+
+    }
+  }
+
+  trait FirstComponent {
+    this: ConditionalComponent =>
+    val first:Task = new ExecutionTask {
+
+      def next: Task = conditional
+
+      def id: String = "1"
+
+      def execute(ctx: WriteableExecutionContext): ExecutionResult = {
+        println("this is the 1")
+        ExecutionOk
+      }
+
+    }
+  }
+
+  object Components extends FirstComponent with SecondComponent with ConditionalComponent with BlockingComponent with Finish1Component with Finish2Component
 
   val workflow = new Workflow {
     def name: String = "nombre"
+
     def version: String = "v1"
-    def firstTask: Task = first
+
+    def firstTask: Task = Components.first
   }
 
   val ctx = new ExecutionContext("BBB", Map.empty)
@@ -152,6 +192,9 @@ object Main extends App {
   val result = workflow.execute(ctx)
 
   println(result)
+
+
+  println(Components.first.tasks)
 
 }
 
@@ -161,17 +204,17 @@ object Main2 extends App {
   import scalaz._
   import Scalaz._
 
-  case class Person(name:String)
+  case class Person(name: String)
 
   object Person {
 
-    implicit val json:JSON[Person] = new JSON[Person] {
+    implicit val json: JSON[Person] = new JSON[Person] {
       def write(value: Person): JValue = JString("putos")
+
       def read(json: JValue): JsonScalaz.Result[Person] = Person("vamo lo pibe").successNel
     }
 
   }
-
 
 
   private val value: JValue = toJSON(Person("pepito"))
@@ -182,6 +225,33 @@ object Main2 extends App {
   private val value2: JsonScalaz.Result[Person] = fromJSON[Person](value)
 
   println(value2)
+
+
+}
+
+
+object Main3 extends App {
+
+  trait Key[T]
+
+  object HolaKey extends Key[String]
+  object NumeroKey extends Key[Int]
+
+
+  val map = Map[Key[_], Any](HolaKey -> "asdad", NumeroKey -> 5)
+
+
+
+  def get[T](key:Key[T]):T = map.get(key).asInstanceOf[T]
+
+  def set[T](key:Key[T], value:T) = ???
+
+
+  println(get(HolaKey))
+
+  println(get(NumeroKey))
+
+
 
 
 
